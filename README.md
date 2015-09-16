50965032d1ab248e820720fd8f19ec81b9697149
demirhancosku <cosku@dokuziki.com>
2015-09-16 20:25:03 +0300

    İnitial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Mysql-Remote-Sync
+Remote database connection is usually slow. This bash script a solution to the problem of the local database sync, while working as a team.
+
+
+Shortly, It's a minimal dump-restore script for MAC OSX - MAMP Mysql Library.
+If you want to use another mysql lib location you can change this line;
+
+```bash
+export PATH=$PATH:/Applications/MAMP/Library/bin/
+```
+
+
+Usage;
+```bash
+    sh mysql-sync.sh
+```
---
+++ mysql-sync.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# OSX Mysql MAMP Lib
+export PATH=$PATH:/Applications/MAMP/Library/bin/
+
+
+# Database Credentials
+host=""
+user=""
+password=""
+remote_db_name=""
+local_db_name=""
+
+# Settings
+backup_path="backups"
+date=$(date +"%d-%b-%Y")
+
+# File Permission
+umask 177
+
+# .SQL dump
+mysqldump --user=$user --password=$password -h $host  $remote_db_name > $backup_path/$local_db_name-$date.sql
+
+# Eski Dosyalarımızı Siliyoruz
+find $backup_path/* -mtime +10 -exec rm {} \;
+
+# Restore Local Database
+mysql -u root -p $local_db_name < $backup_path/$local_db_name-$date.sql
